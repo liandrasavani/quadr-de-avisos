419c86f332f317ae717552c3e95414ca32e44697
Liandra <inavasliandra@gmail.com>
2020-12-13 19:51:06 -0300

    Cadastrar avisos

---
+++ components/avisos/Avisos.js
@@ -0,0 +1,15 @@
+const db = require('../../knexfile.js')
+
+function salvar(aviso) {
+
+    return db.insert(aviso).into('avisos')
+        .then(id => {
+            return { tipo: "sucesso", corpo: "Aviso cadastrado com sucesso! ID " }
+        })
+
+        .catch(err => {
+            return { tipo: "erro", corpo: "Erro" + err }
+        })
+}
+
+module.exports = { salvar }
--- components/avisos/AvisosController.js
+++ components/avisos/AvisosController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+const Aviso = require('./Avisos')
+
 router.get('/', (req,res)=>{
     res.send("Pag Principal")
 })
@@ -9,7 +11,16 @@ router.get('/avisos', (req,res)=>{
 })
 
 router.get('/avisos/novo', (req,res)=>{
-    res.render('formulario_avisos')
+    res.render('formulario_avisos')  
+})
+
+router.post('/avisos/novo', (req,res)=>{
+    const titulo = req.body.titulo
+    const data = req.body.data
+    const mensagem = req.body.mensagem
+
+    const msg = await Aviso.salvar({titulo, data, mensagem})
+    res.render('formulario_avisos', {msg})
 })
 
 module.exports = router
